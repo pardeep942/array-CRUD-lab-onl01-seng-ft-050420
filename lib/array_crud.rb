d8e7d138979792971e6354ba0dd1e16e4b04b149
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 00:42:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,8 +26,8 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  return array[index_number]
+def retrieve_element_from_ndex(array, index_number)
+  ireturn array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
